d645188c28e7bb30d3fe066834d321a70a36166b
hyjang <152379377+gydud25416@users.noreply.github.com>
2025-03-07 11:57:11 +0900

    fix: 그래프 기준 상태 내부에서 관리

--- vue_migration/src/components/my-graph.vue
+++ vue_migration/src/components/my-graph.vue
@@ -14,16 +14,23 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue';
+import { ref } from 'vue';
 
-defineProps<{
-  plusFilter : string
-}>();
+const plusFilter = ref<string>("+1");
 function PlusFilter(value:string){
   console.log(value)
 }
 </script>
 
 <style scoped>
+.wrap_graph{ border:1px solid #555; padding:50px 20px 0 20px; display: flex; gap:30px; justify-content: center;  background:#dee1f3; }
 
+.wrap_graph .graph_box{ position:relative; width:30px; height:180px;  border-radius: 5px 5px 0 0; display: flex; flex-direction: column; align-items: center; background:#fff; box-shadow: 3px 0px 5px rgba(0, 0, 0, 0.2);}
+.wrap_graph .graph_box .graph_item{width:100%; height:0%; background:#5967e4; position:absolute; bottom:0; transition:0.5s; }
+.wrap_graph .graph_box p{ position:absolute; top:100%; padding-top:10px; }
+
+.plusFilter.graph {display: flex; justify-content: flex-end; padding-bottom:5px;}
+.plusFilter.graph li{padding:0 5px;  cursor: pointer;}
+.plusFilter.graph   li.on{color:#5967e4; }
+.plusFilter.graph   li:nth-child(2){ border-left:2px solid #555;}
 </style>
--- vue_migration/src/views/HomeView.vue
+++ vue_migration/src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Button from '@/components/my-button.vue';
+import MyGraph from '@/components/my-graph.vue';
 import MyHeader from '@/components/my-header.vue';
 
 </script>
 
 <template>
     <MyHeader :title="'가계부'"/>
+    <MyGraph/>
     <RouterLink to="/add"><Button :text="'추가하기'" /></RouterLink>
 
 </template>
